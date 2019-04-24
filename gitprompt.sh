88ec9b1fade81b166cf1d9819f8e14bede840d14
Olivier Dupré <dupreolivier@gmail.com>
2019-04-24 15:52:13 +0200

    Added a new K8S theme

--- gitprompt.sh
+++ gitprompt.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Check to make sure that kube-ps1 has been loaded
-if ! type kube_ps1 &> /dev/null; then
-  echo 'gitprompt: Load jonmosco/kube-ps1.' >&2 && return 1
-fi
-
-KUBE_PS1_SYMBOL_ENABLE='false'
-KUBE_PS1_CTX_COLOR='yellow'
-
 function async_run() {
   {
     eval "$@" &> /dev/null
@@ -602,18 +594,6 @@ function updatePrompt() {
     NEW_PROMPT="$(gp_add_virtualenv_to_prompt)${PROMPT_START}$(${prompt_callback})${STATUS_PREFIX}${STATUS}${PROMPT_END}"
   else
     NEW_PROMPT="${EMPTY_PROMPT}"
-  fi
-
-  _polyglot_kube_ps1_prompt_command() {
-    _kube_ps1_update_cache
-    local kube_ps1=$(kube_ps1)
-    [[ -n $kube_ps1 ]] && PS1="\[\033[1m\]$kube_ps1\[\033[0m\]\n$PS1"
-  }
-
-  if [[ $PROMPT_COMMAND != *_polyglot_kube_ps1_prompt_command* ]]; then
-    PROMPT_COMMAND="${PROMPT_COMMAND//_kube_ps1_update_cache\;/}"
-    [[ $PROMPT_COMMAND != *\; ]] && PROMPT_COMMAND+=';'
-    PROMPT_COMMAND+='_polyglot_kube_ps1_prompt_command'
   fi
 
   PS1="${NEW_PROMPT//_LAST_COMMAND_INDICATOR_/${LAST_COMMAND_INDICATOR}${ResetColor}}"
